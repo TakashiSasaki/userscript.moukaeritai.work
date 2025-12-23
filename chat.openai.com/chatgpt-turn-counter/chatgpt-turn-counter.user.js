3336a2de59550bfba6d5d44747f0c086d47de28f
Takashi SASAKI <takashi316@gmail.com>
2025-12-23 14:19:12 +0900

    feat: Add userscript to count turns, images, and code blocks in ChatGPT.

--- chat.openai.com/chatgpt-turn-counter/chatgpt-turn-counter.user.js
+++ chat.openai.com/chatgpt-turn-counter/chatgpt-turn-counter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Turn Counter
 // @namespace    userscript.moukaeritai.work
-// @version      0.2.3
+// @version      0.2.4
 // @description  Count user/assistant turns, images, and code blocks in ChatGPT
 // @author       Takashi Sasaki
 // @homepageURL  https://x.com/TakashiSasaki
