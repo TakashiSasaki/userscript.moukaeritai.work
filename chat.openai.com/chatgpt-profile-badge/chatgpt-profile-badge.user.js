e01aa5479f1e231b626705f87f2ee35dd82ef91c
Takashi SASAKI <takashi316@gmail.com>
2025-12-24 12:31:10 +0000

    chore(chatgpt-profile-badge): Bump version to 0.1.2

--- chat.openai.com/chatgpt-profile-badge/chatgpt-profile-badge.user.js
+++ chat.openai.com/chatgpt-profile-badge/chatgpt-profile-badge.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Profile Badge
 // @namespace    userscript.moukaeritai.work
-// @version      0.1.1
+// @version      0.1.2
 // @description  Add a custom string to the user profile section on ChatGPT.
 // @author       Takashi Sasaki
 // @homepage     https://x.com/TakashiSasaki
