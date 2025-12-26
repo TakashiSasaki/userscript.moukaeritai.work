bc65e4dd79553f77b47916640d3deafdcb7d0ecc
Takashi SASAKI <takashi316@gmail.com>
2025-12-26 15:15:01 +0900

    Update userscript namespace and homepage URL
    Updated the metadata in onamae-block-evil.user.js:
    - Set @namespace to userscript.moukaeritai.work
    - Added @homepageURL for x.com/TakashiSasaki

--- onamae.com/onamae-block-evil/onamae-block-evil.user.js
+++ onamae.com/onamae-block-evil/onamae-block-evil.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         お名前.com 邪悪広告ブロッカー
-// @namespace    https://github.com/TakashiSasaki/world/userscript/onamae.com/onamae-block-evil
+// @namespace    userscript.moukaeritai.work
 // @version      0.1
 // @description  お名前.com Navi の操作を妨げる「邪悪な」広告や確認ポップアップを自動的に非表示にします。
 // @author       Takashi Sasaki
+// @homepageURL  https://x.com/TakashiSasaki
 // @match        https://navi.onamae.com/*
 // @grant        GM_addStyle
 // @run-at       document-start
