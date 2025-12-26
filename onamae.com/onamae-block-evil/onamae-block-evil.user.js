3ecdc93b83296cab5a6b49734fbfbc9924b3dcc2
Takashi SASAKI <takashi316@gmail.com>
2025-12-26 15:13:46 +0900

    Add initial metadata to onamae-block-evil.user.js
    Defined the userscript metadata including name, description, match pattern (navi.onamae.com), and permissions.

---
+++ onamae.com/onamae-block-evil/onamae-block-evil.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         お名前.com 邪悪広告ブロッカー
+// @namespace    https://github.com/TakashiSasaki/world/userscript/onamae.com/onamae-block-evil
+// @version      0.1
+// @description  お名前.com Navi の操作を妨げる「邪悪な」広告や確認ポップアップを自動的に非表示にします。
+// @author       Takashi Sasaki
+// @match        https://navi.onamae.com/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // ここに邪悪な要素をブロックするロジックを記述します。
+})();
