9b88c68ad44be5789949351e274a3c1f5644b979
Takashi SASAKI <takashi316@gmail.com>
2025-12-26 10:28:12 +0900

    Fix Watch Later playlist removal issue
    - Added a missing SVG path for the trash icon specific to the "Watch Later" playlist menu.
    - This ensures the "Remove from Watch Later" option is correctly detected and clicked.
    - Bumped version to 0.1.33.

--- youtube.com/youtube-playlist-saver.user.js
+++ youtube.com/youtube-playlist-saver.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Playlist Saver
 // @namespace    userscript.moukaeritai.work
-// @version      0.1.32
+// @version      0.1.33
 // @description  YouTubeのプレイリストに含まれる動画IDを記録・管理します。
 // @author       Takashi Sasaki
 // @match        *://www.youtube.com/playlist?list=*
@@ -191,7 +191,8 @@
                 // Standard material trash path or variants
                 const trashPaths = [
                     "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z",
-                    "M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H8v15h10V5z"
+                    "M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H8v15h10V5z",
+                    "M19 3h-4V2a1 1 0 00-1-1h-4a1 1 0 00-1 1v1H5a2 2 0 00-2 2h18a2 2 0 00-2-2ZM6 19V7H4v12a4 4 0 004 4h8a4 4 0 004-4V7h-2v12a2 2 0 01-2 2H8a2 2 0 01-2-2Zm4-11a1 1 0 00-1 1v8a1 1 0 102 0V9a1 1 0 00-1-1Zm4 0a1 1 0 00-1 1v8a1 1 0 002 0V9a1 1 0 00-1-1Z"
                 ];
                 if (path) {
                     const d = path.getAttribute('d');
