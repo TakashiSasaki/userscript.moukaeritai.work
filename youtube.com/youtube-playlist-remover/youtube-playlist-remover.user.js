9376fbbd2a9aefce60f53f31ad33cb99fd89512a
Takashi SASAKI <takashi316@gmail.com>
2026-01-08 14:38:35 +0900

    feat: enhance menu loading and visibility wait logic in v0.1.8

--- youtube.com/youtube-playlist-remover/youtube-playlist-remover.user.js
+++ youtube.com/youtube-playlist-remover/youtube-playlist-remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Playlist Remover
 // @namespace    userscript.moukaeritai.work
-// @version      0.1.7
+// @version      0.1.8
 // @description  YouTubeプレイリストで、スクロールして通り過ぎた（Above）動画、またはフィルタリングされた動画を一括削除する機能を提供します。
 // @author       Takashi Sasaki
 // @match        *://www.youtube.com/playlist?*
