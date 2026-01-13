746276d6e6796f58943652f0ddfba947323134c9
Takashi SASAKI <takashi316@gmail.com>
2026-01-13 07:16:39 +0000

    bump: version 0.1.37

--- youtube.com/youtube-playlist-remover/youtube-playlist-remover.user.js
+++ youtube.com/youtube-playlist-remover/youtube-playlist-remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Playlist Remover
 // @namespace    userscript.moukaeritai.work
-// @version      0.1.36
+// @version      0.1.37
 // @description  YouTubeプレイリストで、スクロールして通り過ぎた（Above）動画、またはフィルタリングされた動画を一括削除する機能を提供します。
 // @author       Takashi Sasaki
 // @match        *://www.youtube.com/*
