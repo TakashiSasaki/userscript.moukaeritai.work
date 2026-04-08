618d24c1d982a52ca0005d0b460420613da300a8
Takashi SASAKI <takashi316@gmail.com>
2026-04-08 13:22:47 +0900

    refactor(remover): bump version to 0.1.60 for UI unification

--- youtube.com/youtube-playlist-remover/youtube-playlist-remover.user.js
+++ youtube.com/youtube-playlist-remover/youtube-playlist-remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Playlist Remover
 // @namespace    userscript.moukaeritai.work
-// @version      0.1.60
+// @version      0.1.61
 // @lastModified  2026-04-08
 // @description  YouTubeプレイリストで、スクロールして通り過ぎた（Above）動画、またはフィルタリングされた動画を一括削除する機能を提供します。
 // @antifeature  webRequestBlocking
